262c35995fd632eb5d15ac04f01388480e409a5a
iMassim0 <i.max@live.fr>
2018-04-12 17:49:45 +0200

    OK exo 2 recup nom et cours

--- 2_currencies.rb
+++ 2_currencies.rb
@@ -7,39 +7,43 @@ url = "https://coinmarketcap.com/all/views/all/"
 
 page_1 = Nokogiri::HTML(open(url))
 
-url_2 = "https://coinmarketcap.com/calculator/"
+# Trouver les cours, les rapporter ici, et les traiter pour qu'ils soient propres dans un tableau :
 
-page_2 = Nokogiri::HTML(open(url_2))
+price_not_arranged = []
 
-arr = []
+price_not_arranged << page_1.css('a.price[1][1]').text
 
-arr << page_1.css('a.price[1][1]').text
+price_arranged = price_not_arranged[0].split("$")
 
-temps = arr[0].split("$")
+# Trouver les noms, les rapporter ici, les traiter, les re-traiter pour qu'ils soient propres dans un tableau :
 
-puts temps[1]
+name_not_arranged = []
 
+name_not_arranged << page_1.css('a.currency-name-container')
 
-arr_2 = []
+name_always_not_arranged = []
 
-arr_2 << page_1.css('a.currency-name-container')
-
-temps_2 = (arr_2[0]).to_s
-
-puts (temps_2.to_s).class
-
-kiki = []
-
-temps_2.split('<a class="currency-name-container" href="/currencies/').each do |l|
-  kiki << l
+(name_not_arranged[0]).to_s.split('<a class="currency-name-container" href="/currencies/').each do |line|
+  name_always_not_arranged << line
 end
 
-koko = []
+name_arranged = []
 
-kiki.each do |lulu|
-  koko << lulu.gsub(/a.*/, '/')
-
+name_always_not_arranged.length.times do |i|
+  if !(name_always_not_arranged[i].empty?) && name_always_not_arranged[i].index('/')
+    name_arranged << name_always_not_arranged[i].slice(0..(name_always_not_arranged[i].index('/')-1))
+  end
 end
 
-puts koko
+# Rangement au propre des 2 précédents tableaux dans un Hash propre :
+
+name_and_price = {}
+
+1562.times do |i|
+  name_and_price[name_arranged[i]] = [price_arranged[i+1]]
+end
+
+def
+
+print name_and_price
 
